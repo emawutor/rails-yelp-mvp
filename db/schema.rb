43a4f8b42029f5354a88478264179f5b97d23cab
Emmanuel Mawutor <emawutor@indiana.edu>
2018-08-01 12:22:22 +0100

    created reviews table and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_105924) do
+ActiveRecord::Schema.define(version: 2018_08_01_111631) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "category"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
